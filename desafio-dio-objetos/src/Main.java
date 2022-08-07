9ece9ff67a128cc7f3fd1a6fe9ca22f5a66e4816
Thais Cardoso <thagiat@gmail.com>
2022-08-07 20:32:59 -0300

    Interligando as classes

--- desafio-dio-objetos/src/Main.java
+++ desafio-dio-objetos/src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -12,14 +10,46 @@ public class Main {
         curso.setTitulo("Santander Code");
         curso.setCargaHoraria(90);
 
+        Curso curso2 = new Curso();
+        curso2.setDescricao("Longossssss");
+        curso2.setTitulo("try again");
+        curso2.setCargaHoraria(20);
+
         Mentoria mentoria = new Mentoria();
         mentoria.setData(LocalDate.now());
         mentoria.setDescricao("Viva la vida");
         mentoria.setTitulo("Coldplay");
 
+        /*System.out.println(curso);
+        System.out.println(mentoria);*/
 
-        System.out.println(curso);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setDescricao("existe");
+        bootcamp.setNome("Poke");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Camila:"+ devCamila.getConteudosInscritos());
+
+        Dev devJoy = new Dev();
+        devJoy.setNome("Joy");
+        devJoy.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Joy:"+ devJoy.getConteudosInscritos());
+
+        devJoy.progredir();
+        System.out.println("Conteudos concluidos Joy:"+ devJoy.getConteudosConcluidos());
+
+        devJoy.progredir();
+
+        System.out.println("Conteudos concluidos Joy:"+ devJoy.getConteudosConcluidos());
+        System.out.println("Conteudos concluidos Camila:"+ devCamila.getConteudosConcluidos());
+
+        System.out.println("Xp Joy:" + devJoy.calcularXp());
+        System.out.println("Xp Camila:" + devCamila.calcularXp());
 
     }
 }
--- desafio-dio-objetos/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-dio-objetos/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,10 +11,52 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){}
+    public String getNome() {
+        return nome;
+    }
 
-    public void progredir(){}
-    public void calcularXp(){}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsIncritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else {
+            System.err.println("Voce não esta matriculado em nenhum conteudo");
+        }
+
+    }
+    public double calcularXp(){
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
+    }
+
 
     @Override
     public boolean equals(Object o) {
